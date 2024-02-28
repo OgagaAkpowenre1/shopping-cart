a5ae1495d5b8854336540d7d2c41e0208466ad69
OgagaAkpowenre1 <136248640+OgagaAkpowenre1@users.noreply.github.com>
2024-02-28 05:13:56 +0100

    On branch master  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/cart-tile.jsx 	new file:   src/components/header.jsx 	new file:   src/components/product-tile.jsx 	modified:   src/index.css 	modified:   src/index.js 	new file:   src/pages/cart.jsx 	new file:   src/pages/home.jsx 	new file:   src/store/index.js 	new file:   src/store/slices/cart-slice.js 	new file:   tailwind.config.js

---
+++ src/components/cart-tile.jsx
@@ -0,0 +1,39 @@
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../store/slices/cart-slice";
+
+export default function CartTile({cartItem}){
+	const dispatch = useDispatch();
+
+ function handleRemoveFromCart(){
+  dispatch(removeFromCart(cartItem.id))
+ }
+
+
+ return (
+		<div>
+			<div className="flex items-center p-5 justify-between bg-red-500 mt-5 mb-2 rounded-xl">
+				<div className="flex p-3">
+					<img
+						src={cartItem?.image}
+						className="h-28 rounded-lg"
+						alt={cartItem?.title}
+					/>
+					<div className="ml-10 self-start space-y-5">
+						<h1 className="text-xl text-white font-bold">{cartItem?.title}</h1>
+						<p className="text-white font-extrabold">{cartItem?.price}</p>
+					</div>
+				</div>
+				<div>
+					<button
+						onClick={
+							handleRemoveFromCart
+						}
+						className="bg-red-950 border-2 rounded-lg font-semibold p-4 text-white"
+					>
+      Remove from cart
+					</button>
+				</div>
+			</div>
+		</div>
+ );
+}
